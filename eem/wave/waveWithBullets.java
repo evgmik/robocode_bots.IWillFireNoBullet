2755ba60cd69ee68d6c5b07c7b18c09b3195feb6
Eugeniy Mikhailov <evgmik@gmail.com>
2015-08-08 01:07:01 -0400

    added new wave: waveWithBullets

---
+++ eem/wave/waveWithBullets.java
@@ -0,0 +1,36 @@
+// -*- java -*-
+
+package eem.wave;
+
+import eem.IWillFireNoBullet;
+import eem.bot.*;
+import eem.gun.*;
+import eem.misc.*;
+
+import robocode.Bullet;
+import java.util.LinkedList;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+public class waveWithBullets extends wave {
+	public LinkedList<firingSolution> firingSolutions = new LinkedList<firingSolution>();
+
+	public waveWithBullets( wave w ) {
+		super( w.getFiredBot(), w.getFiredTime(), w.getBulletEnergy() );
+	}
+
+	public void addFiringSolution( firingSolution fS ) {
+		firingSolutions.add(fS);
+	}
+
+	public void onPaint(Graphics2D g, long timeNow) {
+		super.onPaint( g, timeNow );
+		g.setColor(waveColor);
+
+		// draw overall  wave
+		double bulletDistance = getDistanceTraveledAtTime( timeNow ) - 5;
+		for ( firingSolution fS : firingSolutions ) {
+			fS.onPaint( g, bulletDistance );
+		}
+	}
+}
