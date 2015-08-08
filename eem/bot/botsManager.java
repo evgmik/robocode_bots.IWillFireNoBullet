1669eeccb27fb84bdf56d4b9fc4a969de0f2e6ad
Eugeniy Mikhailov <evgmik@gmail.com>
2015-08-08 16:54:52 -0400

    cleaner wave addition on scanned event

--- eem/bot/botsManager.java
+++ eem/bot/botsManager.java
@@ -125,7 +125,8 @@ public class  botsManager {
 			// wave/bullet is fired
 			// FIXME: be smarter about it: check collisions and bullets hits
 			// enemy energy drop detected by one tic later thus -1
-			myBot._gameinfo._wavesManager.add(iBot, myBot.getTime()-1, eDrop );
+			wave w = new wave( iBot, myBot.getTime()-1, eDrop );
+			myBot._gameinfo._wavesManager.add( w );
 		}
 	}
 
